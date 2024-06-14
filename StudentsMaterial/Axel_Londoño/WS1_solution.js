ae2afb24cdabffac90183e98539084ce5abb51a8
Axel Londono <axxl1306@gmail.com>
2024-06-14 14:39:12 -0500

    creacion carpeta

---
+++ StudentsMaterial/Axel_Londoño/WS1_solution.js
@@ -0,0 +1,14 @@
+var raza = "Persa";
+var ojos = "verdes";
+var edad = 3;
+
+if (ojos =="rojos" || (edad > 2 && edad < 5)) {
+
+    console.log("Me lo llevo!");
+} else {
+
+    console.log("Paso!");
+}
+
+var x = 1 + "1";
+console.log(x);
