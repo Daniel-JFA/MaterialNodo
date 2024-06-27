be21bafe66835ce68662bc9a6028bd017b3d22de
leydimadrid <leydi.madrid6@gmail.com>
2024-06-27 17:54:25 -0500

    Avances y práctica sobre el reto de estramypyme

---
+++ StudentsMaterial/Leydi-Madrid/reto1-estramypyme/base-preguntas.js
@@ -0,0 +1,52 @@
+let basePreguntas = [
+  {
+    id: 1,
+    pregunta:
+      "En una escala del 1 al 3, ¿qué tan claro está el propósito fundamental de su empresa para todos los empleados?",
+    opciones: {
+      opcion1: "Nada claro",
+      opcion2: "Algo claro",
+      opcion3: "Muy claro",
+    },
+  },
+  {
+    id: 2,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan comprometidos se sienten los empleados con la misión de la empresa?",
+    opciones: {
+      opcion1: "Nada comprometidos",
+      opcion2: "Poco comprometidos",
+      opcion3: "Muy comprometidos",
+    },
+  },
+  {
+    id: 3,
+    pregunta:
+      "En una escala del 1 al 3, ¿Con qué frecuencia se comunica el propósito de la empresa a los empleados?",
+    opciones: {
+      opcion1: "Nunca",
+      opcion2: "Poco frecuente",
+      opcion3: "Muy frecuente",
+    },
+  },
+  {
+    id: 4,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan alineadas están las decisiones estratégicas de la empresa con su propósito fundamental?",
+    opciones: {
+      opcion1: "Nada alineadas",
+      opcion2: "Poco alineadas",
+      opcion3: "Muy alineadas",
+    },
+  },
+  {
+    id: 5,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan influyente es el propósito de la empresa en la cultura organizacional?",
+    opciones: {
+      opcion1: "Nada influyente",
+      opcion2: "Poco influyente",
+      opcion3: "Muy influyente",
+    },
+  },
+];
