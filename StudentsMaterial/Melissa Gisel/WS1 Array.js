d9014fe8fa808883916e092fcc91d7c42e147bd4
gisel1224 <melissagiseelle@gmail.com>
2024-08-14 10:37:23 -0500

    ws1 corregido

--- StudentsMaterial/Melissa Gisel/WS1 Array.js
+++ StudentsMaterial/Melissa Gisel/WS1 Array.js
@@ -11,7 +11,7 @@
 //    - Descripción: Invierte el orden de los elementos de un vector y muestra el resultado.
 
 
-let total=0,numeros = [1, 2, 3, 4, 5];
+let total=0, numeros = [1, 2, 3, 4, 5];
 for(let i = 0; i < numeros.length; i++) total+=numeros[i];
 console.log(total);
 
@@ -19,6 +19,21 @@ console.log(total);
 console.log(Math.max(1,2,3,4,5))
 
 
+numeros = [1, 2, 3, 4, 5]
 const reversedArray = [...numeros].reverse()
-console.log('originalArray', originalArray) 
+console.log("numeros", numeros) 
 console.log('reversed', reversed) 
+
+
+function numeros(number){
+    let respuesta = [];
+    number = String(number);
+  
+    for(let i = number.length -1 ; i>=0; i--){        
+      respuesta.push(number[i]);
+    }
+    return respuesta;
+  }
+  
+  console.log(numeros(12345))
+  
